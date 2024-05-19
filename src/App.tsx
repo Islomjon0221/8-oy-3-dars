ab7d0cdc5a7005876145f3c5ab557120f26d5301
Islomjon0221 <islomjon20110221@gmail.com>
2024-05-19 15:24:25 +0500

    Completed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import TodoApp from "./componets/Card";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <div className="bg-white h-screen w-full">
+        <TodoApp></TodoApp>
+      </div>
+    </div>
+  );
+}
+
+export default App;
